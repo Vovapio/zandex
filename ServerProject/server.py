fb8187193cd48af6785b82eaccf9a94ed47fe1c9
Vovapio <108529221+Vovapio@users.noreply.github.com>
2025-02-20 19:19:18 +0300

    3

--- ServerProject/server.py
+++ ServerProject/server.py
@@ -18,6 +18,10 @@ def getf():
            'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '<html>'+'<br>'.join(lst) + '<br><html>'
 
+@app.route('/image_mars')
+def image():
+    return '<title>Привет, Марс!</title>'+ '<h1> Жди нас, Марс! </h1>' + "<img src='static/images/IMAGE 2025-02-20 19:10:30.jpg'/>" + '<p> Вот она какая, красная планета. </p>'
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
